7a890cb70144f5767f1ef17c3746adfe147a47c2
Yann-Audin <audinyann@gmail.com>
2023-07-21 15:44:54 -0400

    2023-07-21

---
+++ Anthalgo/anthalgo.py
@@ -0,0 +1,35 @@
+def fetch_data(url):
+    
+    heads = ["id", "book", "fragment", "text_grc", "text_fra", "text_eng", "author", "keywords"]
+    
+    i = 1
+    # loop through all pages
+    while True:
+        # If we've reached the end of the endpoint, break
+        if i > 83:
+            break
+        # Get the next page
+        url = url + str(i)
+        i += 1
+        # Get the page
+        try:
+            response = requests.get(url)
+            break
+        except Exception as e:
+            print("Error:", e)
+            break
+        # Get the json data from the page
+        page_text = response.text
+        page_json = json.loads(page_text)
+        # Loop through the json data of the page
+        for item in page_json:
+            # Loop through each item in the json data
+            for key in heads:
+                # If the key is in the json data
+                if key in item.keys():
+                    # Add the value to the csv file
+                    csv_writer.writerow([item[key]])
+        # Pause for 1 second
+        time.sleep(1)
+        
+        
---
+++ Anthalgo/anthalgo_main.py
@@ -0,0 +1,15 @@
+from projectLibraries import importer
+from Anthalgo import anthalgo
+
+def main():
+    # List of libraries to be imported
+    
+    lib = ["requests", "time", "json"]
+    sub_lib = []
+    lib_as = []
+    
+    importer.import_all_libraries(lib, sub_lib, lib_as)
+    
+    # Import the data from the endpoint
+    
+    url = "https://anthologiagraeca.org/api/passages/?format=json&page="
